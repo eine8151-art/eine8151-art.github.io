f32fa75ce9a1127f8a98225b85ba46ca8ef6155f
eine8151-art <eine8151@gmail.com>
2025-11-26 22:04:58 +0900

    Update api.js

--- js/api.js
+++ js/api.js
@@ -2,7 +2,7 @@
 
 // ⚠ 개발 중: 로컬 서버
 // 나중에 Render에 배포하면 이 값을 "https://xxx.onrender.com" 로 바꾸면 됨
-const API_BASE = 'http://localhost:4000';
+const API_BASE = 'https://text-rpg-backend.onrender.com';
 
 function getToken() {
   return localStorage.getItem('authToken');
@@ -73,3 +73,4 @@ async function login(username, password) {
 async function fetchMe() {
   return apiGet('/api/me');
 }
+
